a4dfedee349a541c89a6e119bc5550df5c3059c3
Vinayak-E <vinayake056@gmail.com>
2025-04-24 18:05:52 +0530

    fix:Cors error

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://wordly.vercel.app'],
+  origin: ['http://localhost:5173', 'https://wordly-sand.vercel.app/'],
   credentials: true,
 }));
 app.use(cookieParser());
